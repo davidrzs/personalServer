e05bb6ac10c1175c24dcf14b1edb53b8d5d97c22
David Zollikofer <david@zollikofer.co>
2018-08-15 16:14:32 +0200

    added functionality

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
+# gem install mongo
+# gem install bson_ext
+
 require 'sinatra/base'
 require 'digest'
-require 
+require 'mongo'
+require 'json/ext' # required for .to_json
 
 
 class App < Sinatra::Base
@@ -8,16 +12,48 @@ class App < Sinatra::Base
     "hello world"
   end
 
-  get "/temperature/:apiKey" do
+  get "/debug" do
+    settings.temperature_db.find.first.to_json
+  end
+
+  get "/temp" do
+    apiKey = params['apiKey']
+    temperature = params['temperature']
+    station = params['station']
+    doc = { time: Time.now, temperature: temperature, station: station }
+    settings.temperature_db.insert_one(doc)
+=begin
      if '80a13a67f485849a3ba8d80dbe87e029886cb90fd6c74a34de00aceb1afe795b26f5cd0743f5c19f1caf239037bd5b3e4047c8201ce5b9167c5111901acc9321' == Digest::SHA512.hexdigest(params['apiKey']).to_s
 
      else
        403
      end
+=end
   end
 
   error 403 do
     'Access forbidden'
+  end
+  helpers do
+    # a helper method to turn a string ID
+    # representation into a BSON::ObjectId
+    def object_id val
+      begin
+        BSON::ObjectId.from_string(val)
+      rescue BSON::ObjectId::Invalid
+        nil
+      end
+    end
+
+    def document_by_id id
+      id = object_id(id) if String === id
+      if id.nil?
+        {}.to_json
+      else
+        document = settings.mongo_db.find(:_id => id).to_a.first
+        (document || {}).to_json
+      end
+    end
   end
 
 end
--- config.ru
+++ config.ru
@@ -1,3 +1,8 @@
 require File.expand_path('app', File.dirname(__FILE__))
 
+configure do
+  db = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'db')
+  set :temperature_db, db[:temperature_db]
+end
+
 run App
